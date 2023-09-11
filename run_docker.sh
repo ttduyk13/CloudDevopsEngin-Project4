1cc37923487c662f651e26319696ad16afa604ff
ttduyk13 <ttduyk13@gmail.com>
2023-09-11 09:02:03 +0700

    implement run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t duytt10-clouddevopsengin-project4  .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 duytt10-clouddevopsengin-project4
