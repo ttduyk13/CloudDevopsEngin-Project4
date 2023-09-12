7579f620a3dfae1b2fc2ac4197f1ab340b7c8b2f
ttduyk13 <ttduyk13@gmail.com>
2023-09-13 01:48:28 +0700

    update make_prediction script

--- make_prediction.sh
+++ make_prediction.sh
@@ -6,13 +6,13 @@ echo "Port: $PORT"
 # POST method predict
 curl -d '{  
    "CHAS":{  
-      "0":0
+      "0":1
    },
    "RM":{  
-      "0":6.575
+      "0":6.6
    },
    "TAX":{  
-      "0":296.0
+      "0":300.0
    },
    "PTRATIO":{  
       "0":15.3
